09f42004e7e54f010bdba8d0255ebbdb38cfaef3
Oswis-pai-02 <oswaldo.do-ch@hotmail.com>
2023-10-24 16:00:35 -0600

    Primer commit en la rama main

--- actualizarTiemposDeEspera.js
+++ actualizarTiemposDeEspera.js
@@ -21,5 +21,5 @@ const tiemposDeEspera = {
   const andenActualizar = "anden1";
   const nuevoTiempoEspera = 7;
   const mensaje = actualizarTiemposDeEspera(andenActualizar, nuevoTiempoEspera);
-  console.log(mensaje);//con comentarios
+  console.log(mensaje);//con comentarios cococococ
   
