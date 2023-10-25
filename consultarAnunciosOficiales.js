acd842e70b15ff2a4beee2507a9fbf3e336676b7
Oswis-pai-02 <oswaldo.do-ch@hotmail.com>
2023-10-24 20:33:14 -0600

    revision hasta consultar afluencia

--- consultarAnunciosOficiales.js
+++ consultarAnunciosOficiales.js
@@ -1,4 +1,5 @@
 const Twit = require('twit');
+const mongoose = require('mongoose');
 
 // Configuración de las credenciales de API de Twitter
 const twitterConfig = {
@@ -7,6 +8,21 @@ const twitterConfig = {
   access_token: 'TU_ACCESS_TOKEN',
   access_token_secret: 'TU_ACCESS_TOKEN_SECRET'
 };
+
+// Conexión a MongoDB Atlas
+mongoose.connect('URL_DE_CONEXION_A_MONGODB_ATLAS', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Definición del esquema para los tweets
+const tweetSchema = new mongoose.Schema({
+  fecha: String,
+  texto: String
+});
+
+// Modelo para los tweets
+const Tweet = mongoose.model('Tweet', tweetSchema);
 
 // Crea un objeto Twit con las credenciales
 const T = new Twit(twitterConfig);
@@ -22,8 +38,19 @@ function consultarAnunciosOficiales() {
       return;
     }
 
-    // Procesar los datos de los tweets
+    // Procesar los datos de los tweets y guardarlos en MongoDB Atlas
     data.forEach(tweet => {
+      const nuevoTweet = new Tweet({
+        fecha: tweet.created_at,
+        texto: tweet.text
+      });
+
+      nuevoTweet.save((err) => {
+        if (err) {
+          console.error('Error al guardar el tweet en MongoDB:', err);
+        }
+      });
+
       console.log(`Fecha: ${tweet.created_at}`);
       console.log(`Texto del tweet: ${tweet.text}`);
       console.log('---------------------------------------------');
@@ -34,4 +61,6 @@ function consultarAnunciosOficiales() {
 // Ejemplo de uso del método consultarAnunciosOficiales()
 consultarAnunciosOficiales();
 
-//Asegúrate de reemplazar 'TU_CONSUMER_KEY', 'TU_CONSUMER_SECRET', 'TU_ACCESS_TOKEN', 'TU_ACCESS_TOKEN_SECRET', y 'ID_DEL_USUARIO' con tus propias credenciales de Twitter y el ID de usuario de la cuenta del metro de la CDMX que desees consultar.
+// Asegúrate de reemplazar 'TU_CONSUMER_KEY', 'TU_CONSUMER_SECRET', 'TU_ACCESS_TOKEN', 'TU_ACCESS_TOKEN_SECRET',
+// 'ID_DEL_USUARIO' y 'URL_DE_CONEXION_A_MONGODB_ATLAS' con tus propias credenciales de Twitter y la URL de
+// conexión a tu base de datos MongoDB Atlas.
